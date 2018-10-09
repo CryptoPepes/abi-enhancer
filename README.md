3f3586a8b27aa71414144bc83e33f2c6e7e46deb
programmerror <programmerror@protonmail.com>
2018-10-09 22:47:11 +0800

    Utility for making abi more useful

---
+++ README.md
@@ -0,0 +1,4 @@
+# ABi-enhancer
+
+Reads a bunch of ABI files, and add the method signature to each function. This makes decoding calldata easy and faster.
+
---
+++ src/enhancer.js
@@ -0,0 +1,25 @@
+import Web3 from "web3";
+
+import fs from "fs";
+
+const web3 = new Web3();
+
+function processABI(abiPath, outputPath) {
+    const abi = JSON.parse(fs.readFileSync(abiPath, 'utf8'));
+
+    for (let i = 0; i < abi.length; i++) {
+        const abiMethod = abi[i];
+        if (abiMethod.type === 'function') {
+            const signature = web3.eth.abi.encodeFunctionSignature(abiMethod);
+            console.log(`Created signature ${signature} for method ${abiMethod.name}.`);
+            // strip off the '0x' prefix
+            abiMethod['sign'] = signature.substring(2);
+        }
+    }
+
+    var json = JSON.stringify(abi);
+    fs.writeFile(outputPath, json, 'utf8', () => console.log("completed!"));
+}
+processABI('abi/CPEP_abi.json', 'out/CPEP_abi.json');
+processABI('abi/sale_abi.json', 'out/sale_abi.json');
+processABI('abi/cozy_abi.json', 'out/cozy_abi.json');
